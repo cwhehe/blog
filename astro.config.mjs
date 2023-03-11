af77c211a91482d44d36e5cf67ae9f8b55cd1c7e
cwhehe <137919031@qq.com>
2023-03-11 14:51:50 +0800

    astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 /** @format */
 
 import { defineConfig } from "astro/config";
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://cwhehe.github.io/",
   integrations: [preact()],
 });
+
+import { defineConfig } from 'astro/config';
+
